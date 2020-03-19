489be1b7efa9d53991ddb2a74b64061b3d7f0bb2
KOMINIARZ Anaïs <kominiarz.anais@gmail.com>
2020-03-19 10:29:10 +0100

    1er TP fini

---
+++ src/classes/Client.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package classes;
+
+import java.util.List;
+
+import interfaces.IClient;
+
+/** Représentation d'un client 
+ *
+ * @author KOMINIARZ Anaïs
+ *
+ */
+public class Client implements IClient{
+
+	@Override
+	public void acheter(List<Object> la) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
